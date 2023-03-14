1fd366ba8c0ef4c102ff6fd26ab7055e325ff5e4
anmolsgcvu <74961789+anmolsgcvu@users.noreply.github.com>
2023-03-14 20:49:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 export default class App extends Component {
   page = 5;
+  api = '89ea725d81304262a61298e69cb47055'
   state = {
     pa:0
   }
@@ -25,14 +26,14 @@ export default class App extends Component {
         progress={this.state.progress}
       />
         <Switch>
-        <Route exact path='/'><News setp={this.setp}  key="general" page={this.page} category="general" country="in"/></Route>
-          <Route exact path="/business"><News setp={this.setp}  key="business" page={this.page} category="business" country="in"/></Route>
-          <Route exact path="/entertainment"><News setp={this.setp}  key="entertainment" page={this.page} category="entertainment" country="in"/></Route>
-          <Route exact path='/general'><News setp={this.setp}  key="general" page={this.page} category="general" country="in"/></Route>
-          <Route exact path='/health'><News setp={this.setp}  key="health" page={this.page} category="health" country="in"/></Route>
-          <Route exact path='/science'><News setp={this.setp}  key="science" page={this.page} category="science" country="in"/></Route>
-          <Route exact path='/sports'><News setp={this.setp}  key="sports" page={this.page} category="sports" country="in"/></Route>
-          <Route exact path='/technology'><News setp={this.setp}  key="technology" page={this.page} category="technology" country="in"/></Route>
+        <Route exact path='/'><News setp={this.setp} api={this.api}  key="home" page={this.page} category="general" country="in"/></Route>
+          <Route exact path="/business"><News setp={this.setp} api={this.api}  key="business" page={this.page} category="business" country="in"/></Route>
+          <Route exact path="/entertainment"><News setp={this.setp} api={this.api}  key="entertainment" page={this.page} category="entertainment" country="in"/></Route>
+          <Route exact path='/general'><News setp={this.setp} api={this.api}  key="general" page={this.page} category="general" country="in"/></Route>
+          <Route exact path='/health'><News setp={this.setp} api={this.api}  key="health" page={this.page} category="health" country="in"/></Route>
+          <Route exact path='/science'><News setp={this.setp} api={this.api}  key="science" page={this.page} category="science" country="in"/></Route>
+          <Route exact path='/sports'><News setp={this.setp} api={this.api}  key="sports" page={this.page} category="sports" country="in"/></Route>
+          <Route exact path='/technology'><News setp={this.setp} api={this.api}  key="technology" page={this.page} category="technology" country="in"/></Route>
         </Switch>
         </Router>
       </div>
